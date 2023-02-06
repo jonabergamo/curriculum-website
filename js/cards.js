736a6eb5aa876b4ac5a34c22ef92fa9652b31c8b
jonabergamo <jonathanbergamo16@gmail.comgit config --listcleargit config --global user.email jonathanbergamo16@gmail.com>
2023-02-06 18:47:13 -0300

    prettier

--- js/cards.js
+++ js/cards.js
@@ -6,27 +6,30 @@ const card = document.querySelector("div.card");
 const videos = [
   {
     title: "Website estilo curriculo",
-    subtitle: "Website ainda em desenvolvimento, consiste em um curriculo criado utilizando HTML, CSS E JAVASCRIPT. Estou estudando torna-lo 100% responsivo para qualquer dispositivo.",
+    subtitle:
+      "Website ainda em desenvolvimento, consiste em um curriculo criado utilizando HTML, CSS E JAVASCRIPT. Estou estudando torna-lo 100% responsivo para qualquer dispositivo.",
     url: "https://www.figma.com/file/P5O1qmWbpnXrOVv37QIXle/Curriculum---Jona-.B?t=rdp6CXC9wWrlmg2B-6",
     git: "https://github.com/jonabergamo/curriculum-website",
     thumb: "./imgs/pastinha.svg",
-    video_id: "GykTLqODQuU"
+    video_id: "GykTLqODQuU",
   },
   {
     title: "Calculadora Simples",
-    subtitle: "Um site onde é possivel acessar uma calculadora que faz operações matematicas simples. Foi feita utilizando HTML, CSS E JAVASCRIPT",
+    subtitle:
+      "Um site onde é possivel acessar uma calculadora que faz operações matematicas simples. Foi feita utilizando HTML, CSS E JAVASCRIPT",
     url: "https://calculadora-jona.netlify.app",
     git: "https://github.com/jonabergamo/Calculadora",
     thumb: "./imgs/pastinha.svg",
-    video_id: "HN1UjzRSdBk"
+    video_id: "HN1UjzRSdBk",
   },
   {
     title: "Website Curriculo (Simples)",
-    subtitle: "Este website foi o primeiro que criei, ele consiste em um curriculo básico feito em HTML e CSS. Também possui um pouco de javascript onde adiciona um scroll bar colorido.",
+    subtitle:
+      "Este website foi o primeiro que criei, ele consiste em um curriculo básico feito em HTML e CSS. Também possui um pouco de javascript onde adiciona um scroll bar colorido.",
     url: "",
     git: "https://github.com/jonabergamo/Site-Curriculo",
     thumb: "./imgs/pastinha.svg",
-    video_id: "rAzHvYnQ8DY"
+    video_id: "rAzHvYnQ8DY",
   },
   {
     title: "Exercicios de Python",
@@ -34,24 +37,20 @@ const videos = [
     url: "",
     git: "https://github.com/jonabergamo/CursoemVideo",
     thumb: "./imgs/pastinha.svg",
-    video_id: "ghTrp1x_1As"
+    video_id: "ghTrp1x_1As",
   },
-
-
 ];
 
-videos.map(video => {
+videos.map((video) => {
   const cardClone = card.cloneNode(true);
   cardClone.setAttribute("id", video.video_id);
   cardClone.querySelector("img").src = video.thumb;
-  cardClone.querySelector("img").src = video.thumb
-  cardClone.querySelector('#url-link').href = video.url;
-  cardClone.querySelector('#git-link').href = video.git;
+  cardClone.querySelector("img").src = video.thumb;
+  cardClone.querySelector("#url-link").href = video.url;
+  cardClone.querySelector("#git-link").href = video.git;
   cardClone.querySelector(".title").innerHTML = video.title;
-  cardClone.querySelector(".info > p.text--low").innerHTML =
-    video.subtitle;
+  cardClone.querySelector(".info > p.text--low").innerHTML = video.subtitle;
   sectionCards.appendChild(cardClone);
 });
 
 card.remove();
-
--- js/index.js
+++ js/index.js
@@ -1,45 +1,43 @@
-const menu  = document.querySelector('.menu');
-const NavMenu  = document.querySelector('.nav-menu');
-var load  = document.querySelector('#load')
+const menu = document.querySelector(".menu");
+const NavMenu = document.querySelector(".nav-menu");
+var load = document.querySelector("#load");
 
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
+menu.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  NavMenu.classList.toggle("ativo");
+});
 
 /*setTimeout(function unwait(){
     load.style.display = "none";
     load.style.overflow = "none"
 }, 5000)*/
 
-window.addEventListener("load", function() {
-    load.style.display = "none";
-})
+window.addEventListener("load", function () {
+  load.style.display = "none";
+});
 
 AOS.init({
-    duration: 1500,
-    offset: 200,
-    once: true,
-  })
-  
+  duration: 1500,
+  offset: 200,
+  once: true,
+});
 
-let progress = document.getElementById("progressbar")
-let totalHeight = document.body.scrollHeight - window.innerHeight
-window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / totalHeight) * 83
-    progress.style.height = progressHeight + "%"
-}
+let progress = document.getElementById("progressbar");
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 83;
+  progress.style.height = progressHeight + "%";
+};
 
 // modo escuro
 
-const html = document.querySelector("html")
-const body = document.querySelector("body")
-const img = document.querySelector("#logo-img")
-const checkbox = document.querySelector('#darkMode')
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+const img = document.querySelector("#logo-img");
+const checkbox = document.querySelector("#darkMode");
 
-checkbox.addEventListener('change', function(){
-    html.classList.toggle('dark-mode')
-    body.classList.toggle('dark-mode')
-    img.style.filter = "invert()"
-})
+checkbox.addEventListener("change", function () {
+  html.classList.toggle("dark-mode");
+  body.classList.toggle("dark-mode");
+  img.style.filter = "invert()";
+});
